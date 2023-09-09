a7b7157ec88001ba1c594338ba97172cdc591d85
ou5ama <lasri.oussama@gmail.com>
2023-09-09 23:47:28 +0100

    create an interface and implement it using a room object

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Room } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -10,7 +11,16 @@ export class RoomsComponent implements OnInit{
 // interpolation variable
 nameOfHotel = "Helton Hotel"
 rating = 5;
-ngOnInit(): void {}
+swimmingPool = false;
+rooms: Room = {
+  availableRooms: 39,
+  bookedRooms: 11,
+  totalRooms: 50
+}
 
+ngOnInit(): void {}
+toggle(){
+  this.swimmingPool = !this.swimmingPool;
+}
 
 }
