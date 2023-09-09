d0f6f5e325ecb96ec0b49d3b44cc4eaa54f6c229
ou5ama <lasri.oussama@gmail.com>
2023-09-09 22:54:12 +0100

    implement the OnInit interface

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.scss']
+  styleUrls: ['./rooms.component.scss'],
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit{
 
+ngOnInit(): void {
+    
+}
 }
