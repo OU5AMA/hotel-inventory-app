c28cf01201fe5be5eb0e7698381646ac412bfb99
ou5ama <lasri.oussama@gmail.com>
2023-09-14 22:47:55 +0100

    to be squashed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,16 @@ import { AppComponent } from './app.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RoomsListComponent } from './rooms/rooms-list/rooms-list.component';
-import { CardsComponent } from './cards/cards.component';
-import { ListOfHotelsComponent } from './list-of-hotels/list-of-hotels.component';
+// import { CardsComponent } from './cards/cards.component';
+// import { ListOfHotelsComponent } from './list-of-hotels/list-of-hotels.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RoomsComponent,
     RoomsListComponent,
-    CardsComponent,
-    ListOfHotelsComponent
+    // CardsComponent,
+    // ListOfHotelsComponent
   ],
   imports: [
     BrowserModule,
